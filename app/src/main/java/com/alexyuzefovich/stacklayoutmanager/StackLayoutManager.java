01458b9e62be8f0b3186140f16651279aee234a9
yuzefovichalex <36435369+yuzefovichalex@users.noreply.github.com>
2019-10-01 13:03:50 +0300

    fixed scrolling to extreme (max top and bottom) positions, added comments

--- app/src/main/java/com/alexyuzefovich/stacklayoutmanager/StackLayoutManager.java
+++ app/src/main/java/com/alexyuzefovich/stacklayoutmanager/StackLayoutManager.java
@@ -41,44 +41,57 @@ public class StackLayoutManager extends RecyclerView.LayoutManager {
 
     @Override
     public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
+        // if two items or more we can scroll it
         if (getItemCount() > 1) {
+            // view fixed in the top
             View firstView = getChildAt(0);
+            // view scrolled and overlapped first view
             View secondView = getChildAt(1);
             if (firstView != null && secondView != null) {
+                // view scrolled after second view
                 View thirdView = getChildAt(2);
                 int secondViewItemPosition = getPosition(secondView);
                 if (thirdView == null && secondViewItemPosition != getItemCount() - 1) {
+                    // we take third view from recycler where position is next after second view item
                     thirdView = addViewFromRecycler(recycler, secondViewItemPosition + 1, false);
                     int viewTop = getDecoratedBottom(secondView);
                     int viewRight = getWidth();
                     int viewBottom = viewTop + getHeight() - 100;
                     layoutDecorated(thirdView, 0, viewTop, viewRight, viewBottom);
                 }
+
+                // count offset
                 int top = secondView.getTop() - dy;
                 int delta = -dy;
+                
+                // scroll down
                 if (dy > 0) {
+                    // if this case is true than second view scrolls up to max top position
+                    // that's why we need to scroll it to 0 (max top position), because top value can be less than 0
                     if (top < 0) {
-                        if (getPosition(firstView) != getItemCount() - 2) {
-                            removeAndRecycleViewAt(0, recycler);
-                        }
-                        delta = 0;
+                        delta = -secondView.getTop();
                     }
-                } else if (dy < 0) {
+                } else if (dy < 0) { // scroll up
                     if (top > getDecoratedBottom(firstView)) {
+                        // if first view is not first in item list we add view under current scrolling view (it will be new first view)
                         if (getPosition(firstView) != 0) {
                             View view = addViewFromRecycler(recycler, getPosition(firstView) - 1, true);
                             int viewRight = getWidth();
                             int viewBottom = getHeight() - 100;
                             layoutDecorated(view, 0, 0, viewRight, viewBottom);
-                        } else { // for blocking scroll of first item
-                            delta = 0;
+                        } else { // for scroll to max bottom position
+                            delta = getDecoratedBottom(firstView) - getDecoratedTop(secondView);
                         }
+                        // remove redundant not visible forth item
                         if (getChildCount() > 3) {
                             removeAndRecycleViewAt(3, recycler);
                         }
                     }
                 }
+
+                // if we can scroll
                 if (delta != 0) {
+                    // scroll all view except first
                     for (int i = 1; i < getChildCount(); i++) {
                         View view = getChildAt(i);
                         if (view != null) {
@@ -87,6 +100,10 @@ public class StackLayoutManager extends RecyclerView.LayoutManager {
                             int viewBottom = view.getBottom() + delta;
                             layoutDecorated(view, 0, viewTop, viewRight, viewBottom);
                         }
+                    }
+                    // if second view completely overlaps first view (first view is not visible now) we remove first view
+                    if (secondView.getTop() == 0 && getPosition(firstView) != getItemCount() - 2) {
+                        removeAndRecycleViewAt(0, recycler);
                     }
                     return dy;
                 }
